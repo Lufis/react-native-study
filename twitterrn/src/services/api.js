835f7968ecf1bf8d00fb0507943a4c8ade9767a4
Daniel da Costa Hora Melo <dmelo.ba@gmail.com>
2019-02-14 09:35:38 -0300

    Exclusão da pasta

--- twitterrn/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    //baseURL: 'https://twitter-chat.herokuapp.com/',
-    baseURL: 'http://192.168.1.124:3000',
-});
-
-export default api;
